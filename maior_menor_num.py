2dcc39044b8399732812273a1a6400ed822493fc
thalya vieira <thalyahadassavieira@gmail.com>
2026-05-06 08:57:06 -0400

    adicionei programa de numero maior ou menor

---
+++ maior_menor_num.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite um numero: "))
+
+if num1>num2:
+    print("O numero {} é maior que o numero {}".format(num1,num2))
+
+elif num1<num2:
+    print("O numero {} é maior que o numero {}".format(num2,num1))
+
+else:
+    print("O numeros sao iguais")
